a66eaf7a43beecc901fd89e63e566f58f9da33b8
Wilson0990 <wlsn0990@gmail.com>
2024-03-07 12:25:31 +0530

    Handle invalid input

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def divide(x, y):
         return "Error! Division by zero."
     else:
         return x / y
-print("----Welcome to Python Calculator----")
+
 print("Select operation:")
 print("1. Add")
 print("2. Subtract")
@@ -22,3 +22,17 @@ print("3. Multiply")
 print("4. Divide")
 
 choice = input("Enter choice (1/2/3/4): ")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if choice == '1':
+    print("Result:", add(num1, num2))
+elif choice == '2':
+    print("Result:", subtract(num1, num2))
+elif choice == '3':
+    print("Result:", multiply(num1, num2))
+elif choice == '4':
+    print("Result:", divide(num1, num2))
+else:
+    print("Invalid input")
