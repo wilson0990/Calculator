da6c7dc984156c6ffcbd59af1d71f87e4f13003f
Wilson0990 <wlsn0990@gmail.com>
2024-03-07 11:15:16 +0530

    Add division function

--- main.py
+++ main.py
@@ -9,7 +9,14 @@ def subtract(x, y):
 def multiply(x, y):
     return x * y
 
+def divide(x, y):
+    if y == 0:
+        return "Error! Division by zero."
+    else:
+        return x / y
+
 print("Select operation:")
 print("1. Add")
 print("2. Subtract")
 print("3. Multiply")
+print("4. Divide")
